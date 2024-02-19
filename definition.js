c33fe7c71952331c4222e6b1dfae66ca9e8edb27
truongphat230799 <taphat1753@gmail.com>
2024-02-19 14:16:28 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -461,7 +461,7 @@ Blockly.Python['stemkit_sound_playtrack'] = function(block) {
     port = 'sound = machine.UART(1, baudrate=9600, rx=pin1.pin, tx=pin14.pin)\n';
   }
   Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
-  Blockly.Python.definitions_['import_sound_player'] = 'from yolobit_sound_player import *'+ port;
+  Blockly.Python.definitions_['import_sound_player'] = 'from stemkit_sound_player import *\n'+ port;
   var number_vol = Blockly.Python.valueToCode(block, 'vol', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
 
